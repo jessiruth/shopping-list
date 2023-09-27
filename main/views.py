870ce086915e58ddd2f0e05aeb60b7c287560139
Jessica Ruth Damai Yanti Manurung <jessicaruth2004@gmail.com>
2023-09-27 16:03:11 +0700

    Tutorial 4

--- main/views.py
+++ main/views.py
@@ -82,3 +82,26 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('main:login')
+
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk = id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
